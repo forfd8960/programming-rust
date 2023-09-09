f4e1df9c6f86a207c60a3d3eca69f192199e0d80
forfd8960 <forfd8960@gmail.com>
2023-09-09 17:22:50 +0800

    [Enums] - Generic Enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -41,6 +41,31 @@ enum Json {
     Object(Box<HashMap<String, Json>>),
 }
 
+// Generic Enum
+
+#[derive(Debug)]
+enum BinaryTree<T> {
+    Empty,
+    NonEmpty(Box<TreeNode<T>>),
+}
+
+#[derive(Debug)]
+struct TreeNode<T> {
+    element: T,
+    left: BinaryTree<T>,
+    right: BinaryTree<T>,
+}
+
+impl<T> TreeNode<T> {
+    fn new(element: T, left: BinaryTree<T>, right: BinaryTree<T>) -> Self {
+        Self {
+            element: element,
+            left: left,
+            right: right,
+        }
+    }
+}
+
 fn main() {
     let tu = TimeUnit::Hours;
     println!("plural: {}", tu.plural());
@@ -52,6 +77,17 @@ fn main() {
 
     let json1 = Json::Boolean(true);
     println!("null: {:?}", json1);
+
+    let bin_tree = BinaryTree::<String>::Empty;
+    println!("{:?}", bin_tree); // Empty
+
+    let left = BinaryTree::<String>::Empty;
+    let right = BinaryTree::<String>::Empty;
+    let tree_node = TreeNode::<String>::new("A".to_string(), left, right);
+    let bin_tree: BinaryTree<String> = BinaryTree::<String>::NonEmpty(Box::new(tree_node));
+
+    // NonEmpty: NonEmpty(TreeNode { element: "A", left: Empty, right: Empty })
+    println!("NonEmpty: {:?}", bin_tree);
 }
 
 fn compare(a: i32, b: i32) -> Ordering {

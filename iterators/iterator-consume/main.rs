b7a2a1ddf4066613b75fa5f1eba6abaa35eeeb18
forfd8960 <forfd8960@gmail.com>
2023-09-16 08:38:53 +0800

    [Iterator]Max By Key, Min By Key

--- iterators/iterator-consume/main.rs
+++ iterators/iterator-consume/main.rs
@@ -39,6 +39,9 @@ fn main() {
     println!("max of data list: {:?}", max_mydata(&data_list));
     // min data list: Some(MyData { name: "Tomato", price: 6 })
     println!("min of data list: {:?}", min_mydata(&data_list)); // Tomato
+
+    max_by_key();
+    min_by_key();
 }
 
 fn sum(n: u32) -> u32 {
@@ -79,4 +82,50 @@ fn max_mydata(data_list: &Vec<MyData>) -> Option<&MyData> {
 
 fn min_mydata(data_list: &Vec<MyData>) -> Option<&MyData> {
     data_list.iter().min_by(compare_data)
+}
+
+fn max_by_key() {
+    let data_list = vec![
+        MyData {
+            name: "Apple".to_string(),
+            price: 10,
+        },
+        MyData {
+            name: "Watermelon".to_string(),
+            price: 30,
+        },
+        MyData {
+            name: "Tomato".to_string(),
+            price: 6,
+        },
+    ];
+
+    // max by key: Some(MyData { name: "Watermelon", price: 30 })
+    let max = data_list.iter().max_by_key(|d| d.price);
+    println!("max by key: {:?}", max);
+}
+
+fn min_by_key() {
+    let data_list = vec![
+        MyData {
+            name: "Apple".to_string(),
+            price: 10,
+        },
+        MyData {
+            name: "Watermelon".to_string(),
+            price: 30,
+        },
+        MyData {
+            name: "Tomato".to_string(),
+            price: 6,
+        },
+        MyData {
+            name: "Tomato".to_string(),
+            price: 6,
+        },
+    ];
+
+    // min by key: Some(MyData { name: "Tomato", price: 6 })
+    let min = data_list.iter().min_by_key(|d| d.price);
+    println!("min by key: {:?}", min);
 }

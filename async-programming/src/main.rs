18e9981e909467800d5b400404085d160045ce8a
forfd8960 <forfd8960@gmail.com>
2023-10-08 23:41:45 +0800

    [AsyncProgramming]Send Async Tcp request

--- async-programming/src/main.rs
+++ async-programming/src/main.rs
@@ -1,4 +1,7 @@
-use async_std::task;
+use async_std::{
+    io::{ReadExt, WriteExt},
+    net, task,
+};
 
 async fn say_hello() {
     println!("Hello, Async Fn");
@@ -6,4 +9,16 @@ async fn say_hello() {
 
 fn main() {
     task::block_on(say_hello())
+}
+
+async fn send_request(host: &str, port: u16, path: &str) -> std::io::Result<String> {
+    let mut socket = net::TcpStream::connect((host, port)).await?;
+    let request = format!("GET {} HTTP/1.1\r\nHost: {}\r\n\r\n", path, host);
+    socket.write_all(request.as_bytes()).await?;
+    socket.shutdown(net::Shutdown::Write)?;
+
+    let mut response = String::new();
+    socket.read_to_string(&mut response).await?;
+
+    Ok(response)
 }

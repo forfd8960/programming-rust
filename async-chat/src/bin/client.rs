a9fa5f697a9fefac2649d440ad552f5440346d6e
forfd8960 <forfd8960@gmail.com>
2023-10-13 21:20:20 +0800

    [AsyncChat] utils::send_as_json, receive_as_json

--- async-chat/src/bin/client.rs
+++ async-chat/src/bin/client.rs
@@ -1,3 +1,23 @@
+use async_chat::utils::{self, ChatResult};
+use async_std::prelude::*;
+use async_std::{io, net};
+
 fn main() {
     println!("client");
+}
+
+async fn send_commands(mut server: net::TcpStream) -> ChatResult<()> {
+    let mut command_lines = io::BufReader::new(io::stdin()).lines();
+    while let Some(cmd_result) = command_lines.next().await {
+        let command = cmd_result?;
+        let request = match parse_command(&command) {
+            Some(request) => request,
+            None => continue,
+        };
+
+        utils::send_as_json(&mut server, &request).await?;
+        server.flush().await?;
+    }
+
+    Ok(())
 }
--- async-chat/src/utils.rs
+++ async-chat/src/utils.rs
@@ -1,14 +1,53 @@
+use async_std::{
+    io::{self, prelude::BufReadExt, BufRead, WriteExt},
+    stream::{Stream, StreamExt},
+};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::error::Error;
-
-pub mod utils;
+use std::sync::Arc;
 
 pub type ChatError = Box<dyn Error + Send + Sync + 'static>;
 pub type ChatResult<T> = Result<T, ChatError>;
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub enum FromClient {
-    Join { group_name: Arc<String> },
+    Join {
+        group_name: Arc<String>,
+    },
+    Post {
+        group_name: Arc<String>,
+        message: Arc<String>,
+    },
 }
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
-pub enum FromServer {}
+pub enum FromServer {
+    Message {
+        group_name: Arc<String>,
+        message: Arc<String>,
+    },
+    Error(String),
+}
+
+pub async fn send_as_json<S, P>(outbound: &mut S, packet: &P) -> ChatResult<()>
+where
+    S: io::Write + Unpin,
+    P: Serialize,
+{
+    let mut json = serde_json::to_string(&packet)?;
+    json.push('\n');
+    outbound.write_all(json.as_bytes()).await?;
+    Ok(())
+}
+
+pub fn receive_as_json<S, P>(inbound: S) -> impl Stream<Item = ChatResult<P>>
+where
+    S: BufRead + Unpin,
+    P: DeserializeOwned,
+{
+    inbound.lines().map(|line_res| -> ChatResult<P> {
+        let line = line_res?;
+        let parsed = serde_json::from_str::<P>(&line)?;
+        Ok(parsed)
+    })
+}

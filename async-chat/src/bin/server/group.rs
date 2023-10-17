c1b0a0c8739057d3a6cf6e2f20b14ccb94b684b4
forfd8960 <forfd8960@gmail.com>
2023-10-17 22:49:18 +0800

    [AsyncChat]Connection serve

--- async-chat/src/bin/server/group.rs
+++ async-chat/src/bin/server/group.rs
@@ -1,6 +1,10 @@
+use async_std::task;
 use std::sync::Arc;
 use tokio::sync::broadcast;
 
+use crate::connection::Outbound;
+
+#[derive(Debug)]
 pub struct Group {
     name: Arc<String>,
     sender: broadcast::Sender<Arc<String>>,
@@ -8,10 +12,26 @@ pub struct Group {
 
 impl Group {
     pub fn new(name: Arc<String>) -> Self {
-        let (sender, receiver) = broadcast::channel(1000);
+        let (sender, _receiver) = broadcast::channel(1000);
         Self {
             name: name,
             sender: sender,
         }
     }
+
+    pub fn join(&self, outbound: Arc<Outbound>) {
+        let receiver = self.sender.subscribe();
+        task::spawn(handle_subscriber(self.name.clone(), receiver, outbound));
+    }
+
+    pub fn post(&self, message: Arc<String>) {
+        let _ignored = self.sender.send(message);
+    }
+}
+
+async fn handle_subscriber(
+    group_name: Arc<String>,
+    mut receiver: broadcast::Receiver<Arc<String>>,
+    outbound: Arc<Outbound>,
+) {
 }

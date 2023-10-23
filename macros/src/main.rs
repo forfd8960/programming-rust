8a998a9e7d2ce65a4f272d2e5efb38b11f042403
forfd8960 <forfd8960@gmail.com>
2023-10-23 19:59:07 +0800

    [Macro] JSON From

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -14,6 +14,41 @@ macro_rules! json {
     (null) => {
         JSON::Null
     };
+    ([$ ($element:tt),*]) => {
+        JSON::Array(vec![$ ( json!($element) ),*])
+    };
+    ({$ ($key:tt : $value:tt),*}) => {
+        JSON::Object(Box::new(vec![
+            $( ($key.to_string(), json!($value)) ),*
+        ].into_iter().collect()))
+    };
+    ( $other:tt )  => {
+        JSON::from($other)
+    };
+}
+
+impl From<bool> for JSON {
+    fn from(value: bool) -> Self {
+        JSON::Boolean(value)
+    }
+}
+
+impl From<i32> for JSON {
+    fn from(value: i32) -> Self {
+        JSON::Number(value as i64)
+    }
+}
+
+impl From<String> for JSON {
+    fn from(value: String) -> Self {
+        JSON::String(value)
+    }
+}
+
+impl<'a> From<&'a str> for JSON {
+    fn from(value: &'a str) -> Self {
+        JSON::String(value.to_string())
+    }
 }
 
 fn main() {
@@ -45,4 +80,35 @@ fn main() {
 
     // json null: Null
     println!("json null: {:?}", json!(null));
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    use crate::JSON;
+
+    #[test]
+    fn json_array_with_element() {
+        let macro_value = json!([{"pitch": 440}]);
+
+        let mut obj = HashMap::new();
+        obj.insert("pitch".to_string(), JSON::Number(440));
+        let expect = JSON::Array(vec![JSON::Object(Box::new(obj))]);
+        assert_eq!(macro_value, expect);
+    }
+
+    #[test]
+    fn json_array_with_element1() {
+        let macro_value = json!(["abc", 123, true, false, null]);
+
+        let expect = JSON::Array(vec![
+            JSON::String("abc".to_string()),
+            JSON::Number(123),
+            JSON::Boolean(true),
+            JSON::Boolean(false),
+            JSON::Null,
+        ]);
+        assert_eq!(macro_value, expect);
+    }
 }
